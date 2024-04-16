1c68e64271f2813f92731532141b10b90d0b75e5
yigitortu <yigittortu287@gmail.com>
2024-04-16 11:48:51 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
-const kullaniciIsmi = prompt("Lütfen adınızı girin:");
-const kullaniciIsmiElementi = document.getElementById("myName");
-kullaniciIsmiElementi.innerHTML = kullaniciIsmi;
+let userName = prompt("Adınızı gir : ")
+
+// document.getElementById("myName").textContent = userName;
+document.querySelector("#myName").textContent = userName;
+
+var currentDate = new Date();
+var currentHour = currentDate.getHours();
+var currentMinutes = currentDate.getMinutes();
+var currentDay = currentDate.getDay();
+
+var daysOfWeek = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"];
+
+document.getElementById("myClock").textContent = 
+ `${currentHour} : ${currentMinutes}   ${daysOfWeek[currentDay]} ` ;
